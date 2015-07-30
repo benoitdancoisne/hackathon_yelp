95116f11dc3ac3524458d7c34dcba2efb86b4afd
benoitd <benoitd@yelp.com>
2015-07-30 14:07:58 -0700

    added is_inside method to shapefile

--- CensusBlock.py
+++ CensusBlock.py
@@ -1,4 +1,4 @@
-import shapefile
+import shapefile_bis as shapefile
 
 class CensusBlock:
     def __init__(self, args):
@@ -7,4 +7,4 @@ class CensusBlock:
 if __name__ == '__main__':
     sf = shapefile.Reader("census2000_blkgrp_nowater/census2000_blkgrp_nowater")
     shapes = sf.shapes()
-    print shapes[5]
+    print shapes[5].is_inside(1,1)
