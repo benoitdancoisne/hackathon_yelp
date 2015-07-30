a7a9b488a55f6364b9865de06fbaa5bd777319b9
benoitd <benoitd@yelp.com>
2015-07-30 13:05:32 -0700

    shape files + playing with it

---
+++ CensusBlock.py
@@ -0,0 +1,10 @@
+import shapefile
+
+class CensusBlock:
+    def __init__(self, args):
+        self.category_counts = {}
+
+if __name__ == '__main__':
+    sf = shapefile.Reader("census2000_blkgrp_nowater/census2000_blkgrp_nowater")
+    shapes = sf.shapes()
+    print shapes[5]
